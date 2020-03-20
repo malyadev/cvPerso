c5ee6cce2dd1645b479ec75e7ec1ec1920f92e72
malyadev <malyadev248@gmail.com>
2020-03-20 18:22:13 +0100

    initialisation projet

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+<header>
+
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
