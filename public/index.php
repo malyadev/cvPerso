0f126777eb624ab839cc9c98552ac4d5761198f9
malyadev <malyadev248@gmail.com>
2020-03-20 18:54:34 +0100

    création fichiers php et css

--- public/index.php
+++ public/index.php
@@ -4,11 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random</title>
+    <title>Maladin Yannice</title>
     <link rel="stylesheet" href="main.css">
+    <meta name="author" content="Yannice Maladin">
 </head>
 
 <body>
+
 <header>
 
 </header>
