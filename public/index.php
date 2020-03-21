33e4f578e07a6ab4fd9ed274f22392ac845228a5
malyadev <malyadev248@gmail.com>
2020-03-21 09:58:05 +0100

    mise en place de la navbar, couleur principales

--- public/index.php
+++ public/index.php
@@ -5,14 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maladin Yannice</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <meta name="author" content="Yannice Maladin">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
 <header>
-    <div id="navbar">
+    <div /*id="navbar"*/>
     <?php
     include "navbar.php";
     ?>
--- public/navbar.php
+++ public/navbar.php
@@ -1,7 +1,10 @@
-<nav class="navbar">
-    <a href="#presentation">Présentation</a>
-    <a href="#formation">Formation</a>
-    <a href="#experience">Expérience</a>
-    <a href="#interets">Intérets</a>
-    <a href="#contact">Contactez-moi</a>
-</nav>
+<div class="navbar">
+    <nav>
+        <a href="#presentation">Présentation</a>
+        <a href="#formation">Formation</a>
+        <a href="#experience">Expérience</a>
+        <a href="#interets">Intérets</a>
+        <a href="#contact">Contactez-moi</a>
+    </nav>
+</div>
+
